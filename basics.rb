2477dbc92f53c95e124eae2529d6e7ad540441d1
codeamac10 <codeamac10@gmail.com>
2016-11-07 12:22:57 -0600

    first mistake

---
+++ basics.rb
@@ -0,0 +1,61 @@
+title = "post"
+description = "this post is about CODEA"
+number_of_likes = 10000
+street = "Av Sonora"
+number = "87"
+state = "Ciudad de Mexico"
+city = "Ciudad de Mexico"
+zip = "25000"
+puts title
+puts description
+puts number_of_likes
+puts street
+puts number
+puts state
+puts city
+puts zip
+num1 = 5
+num2 = 10
+sum = num1 + num2
+puts sum
+difference = num1 - num2
+puts difference
+product = num1 * num2
+puts product
+quotient = num1.to_f / num2.to_f
+puts quotient
+modulus = num1 % num2
+puts modulus.to_f
+
+igual_que = 50 == 50
+igual_que = true
+
+menor_que = 23 < 30
+menor_que = true
+
+mayor_que = 54 > 40
+mayor_que = true
+
+menor_o_igual_que = 45 <= 45
+menor_o_igual_que = false
+
+mayor_o_igual_que = 56 >= 45
+mayor_o_igual_que = false
+
+diferente_que = 34 != 34
+diferente_que = false
+
+valor_1 = (1 < 7 || false) && (true || true)
+valor_1 = true
+
+valor_2 = !false && (!false || 50 != 5**10)
+valor_2 = true
+
+valor_3 = false || !(true || true)
+valor_3 = false
+
+name = "Roberto"
+last_name = "Monroe"
+gender = "M"
+
+puts 'Welcome ' + name.to_s + ', your last name is '  + last_name.to_s + ' and your gender is ' + gender.to_s
